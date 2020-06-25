7daa0303fe5c4abcc2fdcf71de878852fa487047
Suyoag <50969516+Suyoag@users.noreply.github.com>
2020-06-25 15:09:53 -0400

    Add files via upload

---
+++ PackingDimension.py
@@ -0,0 +1,15 @@
+#PACKINGDIMENSION(Sn, r1, r2, )
+#for l ← 1 to ∞do
+#Permute Sn randomly
+#for k ← 1 to 2 do
+#C ← 0 empty set
+#for i ← 1 to n do
+     		#for j←1to|C|do
+#if d(Sn[i],C[j])􏰂<rk then
+#j←n+1
+#if j<n+1then
+     #C ← C ∪{Sn[i]}
+                                     #Lk(l)=log|C|
+		#Dpack=-µ (L2)- µ (L1)/logr2-logr1
+	#If l > 10 and 1.65 (sqrt(sigma^2*L1)+sigma^2(l2))/sqrt(l(logr2-logr1))< dpack*(1-e)/2 then
+	#	Return Dpack
