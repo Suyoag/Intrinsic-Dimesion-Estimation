2b7c9fa688c01af62f6b50668e2af364c21c1cbf
Suyoag <50969516+Suyoag@users.noreply.github.com>
2020-06-26 12:07:08 -0400

    Update PackingDimension.py

--- PackingDimension.py
+++ PackingDimension.py
@@ -13,3 +13,6 @@
 		#Dpack=-µ (L2)- µ (L1)/logr2-logr1
 	#If l > 10 and 1.65 (sqrt(sigma^2*L1)+sigma^2(l2))/sqrt(l(logr2-logr1))< dpack*(1-e)/2 then
 	#	Return Dpack
+def calculator_dimension (r1,r2,Sn,e)
+
+calculator_dimension()
