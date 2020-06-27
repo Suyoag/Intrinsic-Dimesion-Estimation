2751e7dd866e50f2995590f82e09abb3e9d3ed75
Suyoag <50969516+Suyoag@users.noreply.github.com>
2020-06-27 10:14:49 -0400

    Update PackingDimension.py

--- PackingDimension.py
+++ PackingDimension.py
@@ -13,6 +13,16 @@
 # Dpack=-µ (L2)- µ (L1)/logr2-logr1
 # If l > 10 and 1.65 (sqrt(sigma^2*L1)+sigma^2(l2))/sqrt(l(logr2-logr1))< dpack*(1-e)/2 then
 #	Return Dpack
-def calculator_dimension (r1,r2,Sn,e)
+def calculator_dimension(r1, r2, Sn, e):
+    l = 1
+    while True:
+        perm = permutations(Sn)
+        for k in range(1, 2):
+            c = set()
+
+        l += 1
+
+    return (Dpack)
+
 
 calculator_dimension()
