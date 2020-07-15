3449127705e6ef2882eaa08dcea284737dec3031
Suyoag <50969516+Suyoag@users.noreply.github.com>
2020-07-15 10:43:18 -0400

    Update PackingDimension.py

--- PackingDimension.py
+++ PackingDimension.py
@@ -10,6 +10,8 @@
 # If l > 10 and 1.65 (sqrt(sigma^2*L1)+sigma^2(l2))/sqrt(l(logr2-logr1))< dpack*(1-e)/2 then
 #	Return Dpack
 import statistics
+import math
+from statistics import variance
 def calculator_dimension(r1, r2, Sn, e):
     SnLength = len(Sn)
     for l in range (1,9999999):
@@ -21,13 +23,10 @@ def calculator_dimension(r1, r2, Sn, e):
                     if "d(sn[i], C[j]) < rk"
                         j= snlength + 1
                     if j<n+1
-                        C= "c U (sn(i))"
-        if l > 1.65
-                
-                        
-
-
-
+                        C= c.union(sn(i))
+                L(l)=math.log(abs(c))
+            Dpack= statistics.mean(L(l))- statistics.mean(L(l))/math.log(r2)-math.log(r1)
+        if l > 10 and 1.65*math.sqrt(variance(L1)+variance(L2))/math.sqrt(l)*(math.log(r2)-math.log(r1))<Dpack*(1-e)/2                   
     return (Dpack)
 
 
