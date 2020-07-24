a289b8c2b320c328e0feff74cefd9ab0e90b650d
Suyoag <50969516+Suyoag@users.noreply.github.com>
2020-07-24 12:19:24 -0400

    Update PackingDimension.py

--- PackingDimension.py
+++ PackingDimension.py
@@ -7,4 +7,27 @@ def dis2(X, Y):
     return dist
 
 dis2(random.choice(Sn), random.choice(Sn))
+def calculator_dimension(r1, r2, Sn, e, distance):
+    Sn = [1, 2, 3, 4, 5]
+    for l in range(1, 200):
+        perm = permutations(Sn)
+        for k in range(1, 2):
+            c = set()
+            for i in range(1, len(Sn)):
+                for j in range(1, abs(c)):
+                    if distance < 10:
+                        j = len(Sn) + 1
+                    if j < len(Sn) + 1:
+                        C = c.union(random.choice(Sn))
+                L3 = math.log(abs(c))
+                L2 = [88, 42, 43]
+                L1 = [78, 51, 86]
+            Dpack = statistics.mean(L2) - statistics.mean(L1) / math.log(r2) - math.log(r1)
+            if l > 10 and 1.65 * math.sqrt(variance(L1) + variance(L2)) / math.sqrt(l) * (
+                    math.log(r2) - math.log(r1)) < Dpack * (1 - e) / 2:
+                print("Dpack")
+            return Dpack
+
+
+calculator_dimension(4, 8, Sn, 6, distance)
 
