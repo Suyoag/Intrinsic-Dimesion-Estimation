330d78d4baad1efd350f94a5a983e17dd58bdd75
Suyoag <50969516+Suyoag@users.noreply.github.com>
2020-07-26 09:35:13 -0400

    Update PackingDimension.py

--- PackingDimension.py
+++ PackingDimension.py
@@ -1,4 +1,4 @@
-
+import sys
 import statistics
 import math
 from statistics import variance
@@ -13,18 +13,16 @@ def calculator_dimension(r1, r2, Sn, e, distance):
         perm = permutations(Sn)
         for k in range(1, 2):
             c = set()
+            Csize=sys.getsizeof(c)
             for i in range(1, len(Sn)):
-                for j in range(1, abs(c)):
+                for j in range(1, Csize):
                     if distance < r1 or r2:
                         j = len(Sn) + 1
                     if j < len(Sn) + 1:
                         C = c.union(random.choice(Sn))
-                Lk = math.log(abs(c))
+                L1 = math.log(Csize)
             Dpack = statistics.mean(L2) - statistics.mean(L1) / math.log(r2) - math.log(r1)
-            if l > 10 and 1.65 * math.sqrt(variance(L1) + variance(L2)) / math.sqrt(l) * (
-                    math.log(r2) - math.log(r1)) < Dpack * (1 - e) / 2:
+            if l > 10 and 1.65 * math.sqrt(variance(L1) + variance(L2)) / math.sqrt(l) * (math.log(r2) - math.log(r1)) < Dpack * (1 - e) / 2:
             return Dpack
-
-
 calculator_dimension(4, 8, Sn, 6, distance)
 
